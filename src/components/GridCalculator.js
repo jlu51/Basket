617593f19593581d828e30e8a2401dea8f0758d7
Jonathan Lu <jonathanlu00@gmail.com>
2021-06-08 12:22:28 -0700

    Add Delete Specific Row
    Add functionality for deleting a specific row via its row "x" icon. Also added some padding to account for the added styling for button in row

--- src/components/GridCalculator.js
+++ src/components/GridCalculator.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
+import { BsX } from "react-icons/bs";
 
 function GridCalculator() {
    const [rows, setRows] = useState([
       {itemName: "Apples", price: 4.99}, {itemName: "Pears", price: 10.99}]);
    const [header, setHeader] = useState(["Item", "Price", "Anthony", "Jacky", "Jonathan"]);
-   const [item, setItem] = useState("");
-   const [price, setPrice] = useState(0);
+   const [item, setItem] = useState("Roast Beef");
+   const [price, setPrice] = useState("2.99");
 
    function handleAddRow(name) {
       console.log(name);
@@ -14,6 +15,14 @@ function GridCalculator() {
       setRows(prevRows => {
          return {...prevRows, item}
       })
+   }
+
+   function handleRemoveIdxRow(index) {
+      console.log("REMOVING INDEX " + index)
+      const myRows = rows
+      myRows.splice(index, 1)
+      console.log(myRows)
+      setRows(prevRows => ([...myRows]))
    }
 
    function handleAddHeader(name) {
@@ -29,12 +38,18 @@ function GridCalculator() {
          setRows(prevRows => {
             return [...prevRows, {itemName: item, price: price}]
          })
+         // setItem("")
+         // setPrice("")
+         console.log("DEPTH")
       }
+   }
+
+   function testClick() {
+      console.log("CLICKED")
    }
 
    function handleRemoveLastRow() {
       setRows(rows.slice(0, -1))
-
    }
 
    return ( 
@@ -47,33 +62,38 @@ function GridCalculator() {
                      {head}
                   </div>
                ))}
+               <div class="col-1"/>
             </div>
-            {rows.map(row => (
+            {rows.map((row, index) => (
                   <div className="row text-center">
-                     <div class="col bg-light">
+                     <div class="col pt-2 bg-light">
                         {row.itemName}
                      </div>
-                     <div class="col bg-light">
+                     <div class="col pt-2 bg-light">
                         {row.price}
                      </div>
                      {header.slice(2).map(item => (
-                        <div class="col bg-light">
+                        <div class="col pt-2 bg-light">
                            <input type="checkbox"/>
                         </div>
                      ))}
+                     <div class="col-1 bg-light">
+                        <button type="button" class="btn btn-light" onClick={() => handleRemoveIdxRow(index)}>
+                           <BsX/>
+                        </button>
+                     </div>
                   </div>
                ))}
-            
             <div className="container-fullwidth">
                <form>
                   <div class="row ">
                      <div className="col">
                         <label for="inputItem">Item</label>
-                        <input type="text" class="form-control" placeholder="Arby's Roast Beef Sandwich" id="inputItem" onChange={e => setItem(e.target.value)}/>
+                        <input type="text" class="form-control" value={item} placeholder="Arby's Roast Beef Sandwich" id="inputItem" onChange={e => setItem(e.target.value)}/>
                      </div>
                      <div className="col">
                         <label for="inputPrice">Price</label>
-                        <input type="text" class="form-control" placeholder="2.99" id="inputPrice" onChange={e => setPrice(e.target.value)}/>
+                        <input type="text" class="form-control" value={price} placeholder="2.99" id="inputPrice" onChange={e => setPrice(e.target.value)}/>
                      </div>
                      <div className="col">
                         <button type="button" className="mt-4 btn btn-primary" onClick={() => handleAddRow(item, price)}>Add Item</button>
