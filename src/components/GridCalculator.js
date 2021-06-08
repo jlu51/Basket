ed9534378bc577e427e505c2519c01b21e1739bb
Jonathan Lu <jonathanlu00@gmail.com>
2021-06-07 23:55:20 -0700

    Update GridCalculator.js
    Implement add item functionality and fixed some styling issues

--- src/components/GridCalculator.js
+++ src/components/GridCalculator.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 
 function GridCalculator() {
    const [rows, setRows] = useState([
-      {itemName: "Apples"}, {itemName: "Pears"}]);
-   const [header, setHeader] = useState(["Item", "Sam", "Jacky", "Jonathan"]);
+      {itemName: "Apples", price: 4.99}, {itemName: "Pears", price: 10.99}]);
+   const [header, setHeader] = useState(["Item", "Price", "Anthony", "Jacky", "Jonathan"]);
+   const [item, setItem] = useState("");
+   const [price, setPrice] = useState(0);
 
    function handleAddRow(name) {
       console.log(name);
@@ -21,48 +23,67 @@ function GridCalculator() {
       })
    }
 
+   function handleAddRow(item, price) {
+      console.log(price + " " + item);
+      if (item != "" && price != 0) {
+         setRows(prevRows => {
+            return [...prevRows, {itemName: item, price: price}]
+         })
+      }
+   }
+
+   function handleRemoveLastRow() {
+      setRows(rows.slice(0, -1))
+
+   }
+
    return ( 
       <div>
-         <div class="container">
-            <button type="button" className="btn btn-primary" onClick={() => handleAddHeader("Carsonn")}>Primary</button>
-            <div class="row bg-dark text-white">
-               {header.map(item => (
+         <div className="container">
+            <button type="button" className="btn btn-primary" onClick={() => handleAddHeader("Carsonn")}>Add Person</button>
+            <div className="row bg-dark text-white text-center">
+               {header.map(head => (
                   <div class="col">
-                     {item}
+                     {head}
                   </div>
                ))}
             </div>
             {rows.map(row => (
-                  <div className="row">
+                  <div className="row text-center">
                      <div class="col bg-light">
                         {row.itemName}
                      </div>
-                     {header.slice(1).map(item => (
-                        <div class="col">
+                     <div class="col bg-light">
+                        {row.price}
+                     </div>
+                     {header.slice(2).map(item => (
+                        <div class="col bg-light">
                            <input type="checkbox"/>
                         </div>
                      ))}
                   </div>
                ))}
-
-
-            {/* <div class="row">
-               <div class="col bg-light">
-                  Apples
-               </div>
-               <div class="col bg-success">
-                  <input type="checkbox"/>
-               </div>
-               <div class="col bg-success">
-                  <input type="checkbox"/>
-               </div>
-               <div class="col bg-success">
-                  <input type="checkbox"/>
-               </div>
-               <div class="col bg-success">
-                  <input type="checkbox"/>
-               </div> 
-            </div> */}
+            
+            <div className="container-fullwidth">
+               <form>
+                  <div class="row ">
+                     <div className="col">
+                        <label for="inputItem">Item</label>
+                        <input type="text" class="form-control" placeholder="Arby's Roast Beef Sandwich" id="inputItem" onChange={e => setItem(e.target.value)}/>
+                     </div>
+                     <div className="col">
+                        <label for="inputPrice">Price</label>
+                        <input type="text" class="form-control" placeholder="2.99" id="inputPrice" onChange={e => setPrice(e.target.value)}/>
+                     </div>
+                     <div className="col">
+                        <button type="button" className="mt-4 btn btn-primary" onClick={() => handleAddRow(item, price)}>Add Item</button>
+                     </div>
+                     <div className="col">
+                        <button type="button" className="mt-4 btn btn-danger" onClick={handleRemoveLastRow}>Delete Last Item</button>
+                     </div>
+                  </div>
+               </form>
+            </div>
          </div>
       </div>
    );
